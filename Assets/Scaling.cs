cfca6fcbf812cff7ab5de0b353eeb90310e6f88c
Laura P <loparente20@blakeschool.org>
2018-09-06 14:53:41 -0500

    A lot of stuff

--- Assets/Scaling.cs
+++ Assets/Scaling.cs
@@ -7,9 +7,9 @@ public class Scaling : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-    
+
         // Widen the object by 0.1
-        transform.localScale += new Vector3(0.1F, .1f, 0)
+        transform.localScale += new Vector3(0.1F, .1f, 0);
 
     }
 }
